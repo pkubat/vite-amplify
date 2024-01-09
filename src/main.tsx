afee1d9e84bc3c0ab4801fd2b593977402ba1fa1
Philip Kubat <pkubat@gmail.com>
2024-01-09 09:12:44 -0500

    remove amplify be

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { defineBackend } from '@aws-amplify/backend';
+// Remove per AWS Amplify
+// import { defineBackend } from '@aws-amplify/backend';
 
-defineBackend({});
+// defineBackend({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
