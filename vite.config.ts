7dc83e57374aa93c074dd13a4e64e78f1eba0458
Philip Kubat <pkubat@gmail.com>
2024-01-09 09:10:05 -0500

    Fixes for build & amplify

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: {},
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "./runtimeConfig": "./runtimeConfig.browser",
     },
   },
-})
+  build: {
+    // fix issue with build for amplify
+    rollupOptions: {
+      external: ["@aws-amplify/backend", "@aws-amplify/backend-cli"],
+    },
+  },
+});
